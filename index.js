ee08ac1dd2ff26378f74231eb7d1bd0c33a363c7
unknown <robertrey1234@gmail.com>
2019-09-30 18:12:42 -0500

    Put not working. Stuck at 404.

--- index.js
+++ index.js
@@ -71,7 +71,31 @@ server.delete("/api/users/:id", (req, res) => {
     });
 });
 
-server.put("/api/users/:id", (req, res) => {});
+server.put("/api/users/:id", (req, res) => {
+  const { id } = req.params.id;
+  const { name, bio } = req.body;
+  const user = req.body;
+
+  db.update(id, user)
+    .then(user => {
+      if (!name || !bio) {
+        res
+          .status(400)
+          .json({ errorMessage: "Please provide name and bio for the user" });
+      } else if (!user) {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist" });
+      } else {
+        res.status(201).json(user);
+      }
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({ error: "The user information could not be modified" });
+    });
+});
 
 const port = process.env.port || 7000;
 
